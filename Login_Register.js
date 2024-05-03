948dee19a70c762df709dd5d1d157f28f6f0db0a
Emil-Dar <118821869+Emil-Dar@users.noreply.github.com>
2024-05-03 12:06:35 +0800

    initial connection in Firebase

---
+++ Login_Register.js
@@ -0,0 +1,23 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyA_baZ5BOgTRN17sVgEu5ZniAIfJXdk4No",
+    authDomain: "recipe-generator-project-4004a.firebaseapp.com",
+    projectId: "recipe-generator-project-4004a",
+    storageBucket: "recipe-generator-project-4004a.appspot.com",
+    messagingSenderId: "197530831320",
+    appId: "1:197530831320:web:7f6075fbb9c48538520aad",
+    measurementId: "G-VZ5SSQ87ZJ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
